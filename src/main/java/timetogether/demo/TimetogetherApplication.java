3c93ea57c5e4d9f47bd3edc27842c79349a2f586
Hyeri1ee <haerizian10@gmail.com>
2024-10-28 15:51:16 +0900

    env: 프로젝트 기초 설정

---
+++ src/main/java/timetogether/demo/TimetogetherApplication.java
@@ -0,0 +1,13 @@
+package timetogether.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetogetherApplication.class, args);
+	}
+
+}
