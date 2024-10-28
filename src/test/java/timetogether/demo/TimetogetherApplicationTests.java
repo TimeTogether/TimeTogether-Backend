771605cf3e0a417054acba1f6b8d1e89c2b749d8
Hyeri1ee <haerizian10@gmail.com>
2024-10-28 15:51:16 +0900

    env: 프로젝트 기초 설정

---
+++ src/test/java/timetogether/demo/TimetogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package timetogether.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
