0ff146b71c236c5c975da188e5fda3be68cad1d8
Hyeri1ee <haerizian10@gmail.com>
2024-11-02 11:32:08 +0900

    bug: 배포 appspec.yml 오류2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,4 +28,8 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+  -Duser.timezone=Asia/Seoul \
+  -Xmx1024m \
+  -Dspring.profiles.active=prod \
+  $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
