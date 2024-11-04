38e91031ea3873a5068fc196efb02a9d57fb1657
Hyeri1ee <haerizian10@gmail.com>
2024-11-05 00:53:57 +0900

    feat: GroupCreateResponseDto 추가

---
+++ src/main/java/timetogether/domain/group/service/GroupService.java
@@ -0,0 +1,13 @@
+package timetogether.domain.group.service;
+
+import org.springframework.stereotype.Service;
+import timetogether.domain.group.dto.GroupCreateRequestDto;
+import timetogether.domain.group.dto.GroupCreateResponseDto;
+
+@Service
+public class GroupService {
+  //요거 수정중
+  public GroupCreateResponseDto createGroup(String socialId, GroupCreateRequestDto request) {
+    return null;
+  }
+}
