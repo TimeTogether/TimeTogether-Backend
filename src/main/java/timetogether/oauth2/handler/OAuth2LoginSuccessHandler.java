5cf2418f648443156e7d1bf1dfca357d6e5e28f0
Hyeri1ee <haerizian10@gmail.com>
2024-11-20 18:51:32 +0900

    fix: 프론트와 통신 후 오류 제거

--- src/main/java/timetogether/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/timetogether/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -82,8 +82,8 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         jwtService.updateRefreshToken(socialId, refreshToken); // 사용자의(소셜아이디) 리프레시 토큰를 db에 저장
         //String redirectUrl = "http://172.20.10.4:3000/oauth2/redirect?access_token=" + accessToken;
 
-//        String redirectUrl = "http://192.168.233.242:3000/login/oauth2/redirect?access_token=" + accessToken + "&refresh_token=" + refreshToken;
-//        response.sendRedirect(redirectUrl);
+        String redirectUrl = "http://192.168.233.242:3000/login/oauth2/redirect?access_token=" + accessToken + "&refresh_token=" + refreshToken;//Front ip 로 설정
+        response.sendRedirect(redirectUrl);
 
 //        HttpSession session = request.getSession();
 //        log.info("Session ID in loginSuccess: {}", session.getId());
